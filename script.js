ee9181ab264746d5b879e1dd2ad549ca9bf2e707
unknown <jareduyong@gmail.com>
2022-03-13 01:02:10 +0800

    optimization

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
-$(document).ready(function () {
+// $(document).ready(function () {
+//
+//   $(function(){
+//       $(window).scroll(function(){
+//         // console.log($(this).scrollTop())
+//           if($(this).scrollTop() <= 00){
+//               $('.section1interactive').css('width', '100%');
+//               $('.Section1Content').css('display', 'none');
+//           }
+//           else if($(this).scrollTop() <= 100) {
+//               $('.section1interactive').css('width', '200%');
+//           }
+//           else if($(this).scrollTop() <= 200) {
+//               $('.section1interactive').css('width', '400%');
+//           }
+//           else if($(this).scrollTop() <= 300) {
+//               $('.section1interactive').css('width', '800%');
+//           }
+//           else if($(this).scrollTop() <= 400) {
+//             $('.Section1').css('position', 'fixed');
+//               $('.section1interactive').css('width', '1600%');
+//               $('.Section1Content').css('opacity', '0');
+//           }
+//           else if($(this).scrollTop() <= 500) {
+//               $('.Section1').css('position', 'fixed');
+//               $('.section1interactive').css('display', 'block');
+//               $('.Section1Content').css('display', 'inherit');
+//               $('.Section1Content').css('opacity', '1');
+//           }
+//           else if($(this).scrollTop() > 500) {
+//               $('.section1interactive').css('display', 'none');
+//               $('.Section1').css('position', 'relative');
+//
+//           }
+//       });
+//   });
+// });
 
-  $(function(){
-      $(window).scroll(function(){
-        // console.log($(this).scrollTop())
-          if($(this).scrollTop() <= 00){
-              $('.section1interactive').css('width', '100%');
-              $('.Section1Content').css('display', 'none');
-          }
-          else if($(this).scrollTop() <= 100) {
-              $('.section1interactive').css('width', '200%');
-          }
-          else if($(this).scrollTop() <= 200) {
-              $('.section1interactive').css('width', '400%');
-          }
-          else if($(this).scrollTop() <= 300) {
-              $('.section1interactive').css('width', '800%');
-          }
-          else if($(this).scrollTop() <= 400) {
-              $('.section1interactive').css('width', '1600%');
-              $('.Section1Content').css('opacity', '0');
-          }
-          else if($(this).scrollTop() <= 500) {
-              $('.section1interactive').css('display', 'block');
-              $('.Section1').css('position', 'fixed');
-              $('.Section1Content').css('display', 'inherit');
-              $('.Section1Content').css('opacity', '1');
-          }
-          else if($(this).scrollTop() > 500) {
-              $('.section1interactive').css('display', 'none');
-              $('.Section1').css('position', 'relative');
 
-          }
-      });
-  });
-});
+
+//  SCROLL --------------
+
+window.onscroll = function (e) {
+  console.log(window.scrollY)
+}
 
 
 
 
+// COUNTER ----------------------
 var countDownDate = new Date("April 2, 2022 15:00:00").getTime();
 
 var x = setInterval(function() {
