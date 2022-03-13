5249d98344fe94c2744242992c740b64a30435bc
unknown <jareduyong@gmail.com>
2022-03-13 23:36:46 +0800

    All pages

---
+++ graveyard.js
@@ -0,0 +1,39 @@
+// $(document).ready(function () {
+//
+//   $(function(){
+//       $(window).scroll(function(){
+//         // console.log($(this).scrollTop())
+//           if($(this).scrollTop() <= 00){
+//               $('.section1interactive').css('width', '100%');
+//               $('.Section1Content').css('display', 'none');
+//           }
+//           else if($(this).scrollTop() <= 100) {
+//               $('.section1interactive').css('width', '200%');
+//           }
+//           else if($(this).scrollTop() <= 200) {
+//               $('.section1interactive').css('width', '400%');
+//           }
+//           else if($(this).scrollTop() <= 300) {
+//               $('.section1interactive').css('width', '800%');
+//           }
+//           else if($(this).scrollTop() <= 400) {
+//             $('.Section1').css('position', 'fixed');
+//               $('.section1interactive').css('width', '1600%');
+//               $('.Section1Content').css('opacity', '0');
+//           }
+//           else if($(this).scrollTop() <= 500) {
+//               $('.Section1').css('position', 'fixed');
+//               $('.section1interactive').css('display', 'block');
+//               $('.Section1Content').css('display', 'inherit');
+//               $('.Section1Content').css('opacity', '1');
+//           }
+//           else if($(this).scrollTop() > 500) {
+//               $('.section1interactive').css('display', 'none');
+//               $('.Section1').css('position', 'relative');
+//
+//           }
+//       });
+//   });
+// });
+
+//  SCROLL --------------
--- script.js
+++ script.js
@@ -1,51 +1,50 @@
-// $(document).ready(function () {
-//
-//   $(function(){
-//       $(window).scroll(function(){
-//         // console.log($(this).scrollTop())
-//           if($(this).scrollTop() <= 00){
-//               $('.section1interactive').css('width', '100%');
-//               $('.Section1Content').css('display', 'none');
-//           }
-//           else if($(this).scrollTop() <= 100) {
-//               $('.section1interactive').css('width', '200%');
-//           }
-//           else if($(this).scrollTop() <= 200) {
-//               $('.section1interactive').css('width', '400%');
-//           }
-//           else if($(this).scrollTop() <= 300) {
-//               $('.section1interactive').css('width', '800%');
-//           }
-//           else if($(this).scrollTop() <= 400) {
-//             $('.Section1').css('position', 'fixed');
-//               $('.section1interactive').css('width', '1600%');
-//               $('.Section1Content').css('opacity', '0');
-//           }
-//           else if($(this).scrollTop() <= 500) {
-//               $('.Section1').css('position', 'fixed');
-//               $('.section1interactive').css('display', 'block');
-//               $('.Section1Content').css('display', 'inherit');
-//               $('.Section1Content').css('opacity', '1');
-//           }
-//           else if($(this).scrollTop() > 500) {
-//               $('.section1interactive').css('display', 'none');
-//               $('.Section1').css('position', 'relative');
-//
-//           }
-//       });
-//   });
-// });
-
-
-
-//  SCROLL --------------
-
 window.onscroll = function (e) {
-  console.log(window.scrollY)
+
+  document.getElementsByClassName("twitframe")[0].style.height = document.getElementsByClassName("Topleft3_2")[0].clientHeight + document.getElementsByClassName("Botleft3_2")[0].clientHeight + 40 + "px";
+  // console.log(document.getElementsByClassName("left3_2")[0].clientHeight)
+  // console.log(window.scrollY)
+  if(window.scrollY <= 00){
+    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
+    document.getElementsByClassName("section1interactive")[0].style.width = "100%";
+    document.getElementsByClassName("Section1Content")[0].style.display = "none";
+  }
+  else if(window.scrollY <= 100){
+    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
+    document.getElementsByClassName("section1interactive")[0].style.width = "200%";
+  }
+  else if(window.scrollY <= 200){
+    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
+    document.getElementsByClassName("section1interactive")[0].style.width = "400%";
+  }
+  else if(window.scrollY <= 300){
+    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
+    document.getElementsByClassName("section1interactive")[0].style.width = "800%";
+  }
+  else if(window.scrollY <= 400){
+    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
+    document.getElementsByClassName("section1interactive")[0].style.width = "1600%";
+    document.getElementsByClassName("Section1Content")[0].style.opacity = "0";
+  }
+  else if(window.scrollY <= 500){
+    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
+    document.getElementsByClassName("section1interactive")[0].style.width = "1600%";
+    document.getElementsByClassName("Section1Content")[0].style.opacity = "1";
+    document.getElementsByClassName("Section1Content")[0].style.display = "inherit";
+  }
+
+
+  if(window.scrollY <= 500){
+    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
+    document.getElementsByClassName("section1interactive")[0].style.display = "block";
+
+  }
+  else if(window.scrollY < 500){
+    document.getElementsByClassName("section1interactive")[0].style.display = "none";
+    document.getElementsByClassName("Section1Content")[0].style.display = "inherit";
+    document.getElementsByClassName("Section1Content")[0].style.opacity = "1";
+    // document.getElementsByClassName("Section1")[0].style.position = "relative";
+  }
 }
-
-
-
 
 // COUNTER ----------------------
 var countDownDate = new Date("April 2, 2022 15:00:00").getTime();
