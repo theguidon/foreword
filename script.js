567ed85c55fdf2b39bb937e4aadf0342b301de5c
unknown <jareduyong@gmail.com>
2022-03-21 00:23:36 +0800

    Mobile first page

--- script.js
+++ script.js
@@ -2,42 +2,43 @@ window.onscroll = function (e) {
   // console.log(document.getElementsByClassName("left3_2")[0].clientHeight)
   // console.log(window.scrollY)
   if(window.scrollY <= 00){
-    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
     document.getElementsByClassName("section1interactive")[0].style.width = "100%";
+    document.getElementsByClassName("section1interactive")[1].style.width = "150%";
     document.getElementsByClassName("Section1Content")[0].style.display = "none";
   }
   else if(window.scrollY <= 100){
-    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
     document.getElementsByClassName("section1interactive")[0].style.width = "200%";
+    document.getElementsByClassName("section1interactive")[1].style.width = "250%";
   }
   else if(window.scrollY <= 200){
-    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
     document.getElementsByClassName("section1interactive")[0].style.width = "400%";
+    document.getElementsByClassName("section1interactive")[1].style.width = "500%";
   }
   else if(window.scrollY <= 300){
-    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
     document.getElementsByClassName("section1interactive")[0].style.width = "800%";
+    document.getElementsByClassName("section1interactive")[1].style.width = "950%";
   }
   else if(window.scrollY <= 400){
-    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
     document.getElementsByClassName("section1interactive")[0].style.width = "1600%";
+    document.getElementsByClassName("section1interactive")[1].style.width = "4000%";
     document.getElementsByClassName("Section1Content")[0].style.opacity = "0";
   }
   else if(window.scrollY <= 500){
-    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
     document.getElementsByClassName("section1interactive")[0].style.width = "1600%";
+    document.getElementsByClassName("section1interactive")[1].style.width = "4000%";
     document.getElementsByClassName("Section1Content")[0].style.opacity = "1";
     document.getElementsByClassName("Section1Content")[0].style.display = "inherit";
   }
 
 
   if(window.scrollY <= 500){
-    // document.getElementsByClassName("Section1")[0].style.position = "fixed";
     document.getElementsByClassName("section1interactive")[0].style.visibility = "visible";
+    document.getElementsByClassName("section1interactive")[1].style.visibility = "visible";
 
   }
   else if(window.scrollY > 500){
     document.getElementsByClassName("section1interactive")[0].style.visibility = "hidden";
+    document.getElementsByClassName("section1interactive")[1].style.visibility = "hidden";
     document.getElementsByClassName("Section1Content")[0].style.display = "inherit";
     document.getElementsByClassName("Section1Content")[0].style.opacity = "1";
     // document.getElementsByClassName("Section1")[0].style.position = "relative";
@@ -55,8 +56,8 @@ window.onload = function (e) {
 
 
 // COUNTER ----------------------
-var countDownDate = new Date("April 8, 2022 16:00:00").getTime();
-// var countDownDate = new Date("Mar 19, 2022 22:12:00").getTime();
+// var countDownDate = new Date("April 8, 2022 16:00:00").getTime();
+var countDownDate = new Date("Mar 20, 2022 22:24:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
@@ -73,7 +74,7 @@ var x = setInterval(function() {
   document.getElementsByClassName("CountdownClass")[3].innerHTML = ":" + seconds.toLocaleString(undefined,{minimumIntegerDigits: 2}) + "&nbsp;";
   // If the count down is finished, write some text
   if (distance < 0) {
-    clearInterval(x);
+    // clearInterval(x);
     document.getElementsByClassName("CountdownClass")[0].innerHTML = "00";
     document.getElementsByClassName("CountdownClass")[1].innerHTML = ":00";
     document.getElementsByClassName("CountdownClass")[2].innerHTML = ":00";
@@ -81,6 +82,16 @@ var x = setInterval(function() {
     document.getElementById("livestream").style.visibility = "visible";
     document.getElementById("livestream").style.opacity = "1";
     document.getElementById("livestream").style.transform = "translate(0px)";
+    if (document.body.clientWidth < 976) {
+      document.getElementById("Countdown").style.height = "0px";
+      document.getElementById("Countdown").style.opacity = "0";
+      document.getElementById("livestream").style.height = "200px";
+    }
+    else {
+      document.getElementById("Countdown").style.height = "100px";
+      document.getElementById("Countdown").style.opacity = "1";
+      document.getElementById("livestream").style.height = "378px";
+    }
 
   }
 }, 1000);
